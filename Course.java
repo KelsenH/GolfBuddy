71461bd6f1b039ab347b0689a5e6cb2765eaf0d1
Kelsen <kelsenhazelwood@icloud.com>
2016-11-23 17:45:02 -0500

    Course.java: need to figure out logic for getBestScore and adding and removing scores

--- Course.java
+++ Course.java
@@ -1,6 +1,6 @@
 //Course.java
 
-public class Course {
+public class Course implements Serializable {
   private String courseName;
   private int[18] pars;
   private float courseRating;
@@ -10,13 +10,37 @@ public class Course {
 
   Course () {
     courseName = "";
-    courseRating = 70;;
+    courseRating = 70;
     slopeRating = 125;
     headScore = new Score ();
     bestScore = null;
   } //end constructor
 
-  Course () {
+  Course (String courseName) {
+    this.courseName = courseName;
+    courseRating = 70;
+    slopeRating = 125;
+    headScore = new Score ();
+    bestScore = null;
+  } //end constructor
+
+  public void setCourseName (String courseName) {
+    this.courseName = courseName;
+  } //end setCourseName
+
+  public void setPar (int holeNum, int par) {
+    pars[holeNum - 1] = par;
+  } //end setPar
+
+  public String getCourseName () {
+    return courseName;
+  } //end getCourseName
+
+  public int getPar (int holeNum) {
+    return pars[holeNum - 1];
+  } //end getPar 
+
+  public int getBestScore () {
     
   }
 } //end class
